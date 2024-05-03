85e675cc2f196857fd1610b3a7c49c77b27aa349
Maxim Sklyanin <maxsklyanin@gmail.com>
2024-05-03 21:15:47 +0300

    add files config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,34 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import webpack from "webpack";
+import { BuildOptions } from "./types/config";
+
+const buildLoaders = (options: BuildOptions): webpack.RuleSetRule[] => {
+  const typescriptLoader = {
+    test: /\.tsx?$/,
+    use: "ts-loader",
+    exclude: /node_modules/,
+  };
+
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      options.isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+      {
+        loader: "css-loader",
+        options: {
+          modules: {
+            auto: (resPath: string) => resPath.includes(".module."),
+            localIdentName: options.isDev
+              ? "[path][name]__[local]--[hash.base64:5]"
+              : "[hash.base64:8]",
+          },
+        },
+      },
+      "sass-loader",
+    ],
+  };
+
+  return [typescriptLoader, cssLoader];
+};
+
+export default buildLoaders;
